99eea33930ac7ec3cb192fd78976a7b162a77b0a
Kosta155 <ninik@sheridancollege.ca>
2023-03-17 13:08:40 -0400

    Starting code adding methods and classes

---
+++ src/ca/sheridancollege/project/StartGame.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kosti
+ */
+public class StartGame {
+    
+    public static void main(String[] args)
+    {
+    
+    }
+    
+    public ArrayList<UnoCard> generateDeck()
+    {
+        GroupOfCards myCards = new GroupOfCards(60);
+        ArrayList<UnoCard> deck = myCards.getCards();
+        
+        
+        
+        
+        
+        return deck;
+    }
+    }
